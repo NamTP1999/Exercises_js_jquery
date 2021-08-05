c2e764be2b281e73ed1415aff983c53c3e91d2d6
nampt <nampt@runsystem.net>
2021-08-05 14:31:24 +0700

    clean code

--- Javascript/assets/js/select_item.js
+++ Javascript/assets/js/select_item.js
@@ -1,47 +1,47 @@
-function selectItem(){	
+function selectItem() {	
 	/* get id of id of items*/
-	var select = document.getElementById('id_items');
+	const select = document.getElementById('id_items');
 	var option = select.options[select.selectedIndex].value;
 
 	/* get id of province*/
-	var province = document.getElementById('province');
+	const province = document.getElementById('province');
 
 	/* get length option of province*/
-	var countProvince = province.options.length;
+	const countProvince = province.options.length;
+	
 	/* set highlight */
-	for(let i = 0; i < countProvince; i++){
+	for (let i = 0; i < countProvince; i++) {
 		/* select item*/
-		if(option == i+1){
+		if (option == i + 1) {
 			province.options[i].style.color = "white";
 			province.options[i].style.background = "gray";
-		}else{
+		} else {
 			province.options[i].style.color = "black";
 			province.options[i].style.background = "none";
 		}
 
 		/* select even number*/
-		if(option == 7){
-			if((i+1) % 2 == 0){
+		if (option == 7) {
+			if ((i + 1) % 2 == 0) {
 				province.options[i].style.color = "white";
 				province.options[i].style.background = "gray";
 			}
 		}
+
 		/* select odd*/
-		if(option == 8){
-			if((i+1) % 2 != 0){
+		if (option == 8) {
+			if ((i + 1) % 2 != 0) {
 				province.options[i].style.color = "white";
 				province.options[i].style.background = "gray";
 			}
 		}
 
 		/* reset*/
-		if(option == 9){
+		if (option == 9) {
 			province.options[i].style.color = "black";
 			province.options[i].style.background = "none";
 		}
 
 	}
-
-
 
 }
