b6c26b6f9997d19b46dc1e7af9424dbf76b271d8
nampt <nampt@runsystem.net>
2021-08-05 17:33:23 +0700

    clean code

--- Javascript/assets/js/select_item.js
+++ Javascript/assets/js/select_item.js
@@ -21,7 +21,7 @@ function selectItem() {
 		}
 
 		/* select even number*/
-		if (option == 7) {
+		if (option == countProvince + 1 ) {
 			if ((i + 1) % 2 == 0) {
 				province.options[i].style.color = "white";
 				province.options[i].style.background = "gray";
@@ -29,7 +29,7 @@ function selectItem() {
 		}
 
 		/* select odd*/
-		if (option == 8) {
+		if (option == countProvince + 2) {
 			if ((i + 1) % 2 != 0) {
 				province.options[i].style.color = "white";
 				province.options[i].style.background = "gray";
@@ -37,7 +37,7 @@ function selectItem() {
 		}
 
 		/* reset*/
-		if (option == 9) {
+		if (option == 3) {
 			province.options[i].style.color = "black";
 			province.options[i].style.background = "none";
 		}
